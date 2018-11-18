4db834496ed2ab69298cbad5b5a9e2bea71123f6
Tindur20 <Sigsteinn@gmail.com>
2018-11-18 21:06:30 +0000

    Add files via upload

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    public float Speed;
+    public float distance;
+    private bool movingRight = false;
+    public Transform groundDetection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * Speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        if (groundInfo.collider == true)
+        {
+            if (movingRight == false)
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                movingRight = true;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = false;
+            }
+        }
+
+    }
+
+}
